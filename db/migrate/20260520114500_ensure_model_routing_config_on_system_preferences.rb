c05465d57ee68029da2a3c2155abcd961bdb3f70
Mirko Mignini <mirko.mignini@gmail.com>
2026-05-20 22:10:55 +0200

    chore: Remove migration for model routing config from system preferences

--- db/migrate/20260520114500_ensure_model_routing_config_on_system_preferences.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class EnsureModelRoutingConfigOnSystemPreferences < ActiveRecord::Migration[8.1]
-  def change
-    return if column_exists?(:system_preferences, :model_routing_config)
-
-    add_column :system_preferences, :model_routing_config, :jsonb, default: {}, null: false
-  end
-end
